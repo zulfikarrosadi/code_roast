3710de0f72bd817e02f13b0924757e24c0141ebe
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2025-02-11 10:11:35 +0700

    feat(logger): setup log middleware using slog

--- api/main.go
+++ api/main.go
@@ -1,13 +1,55 @@
 package main
 
 import (
+	"log/slog"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogStatus:    true,
+		LogRequestID: true,
+		LogMethod:    true,
+		LogLatency:   true,
+		LogURIPath:   true,
+		LogUserAgent: true,
+		LogRemoteIP:  true,
+		LogError:     true,
+		HandleError:  true,
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			c.Set("requestId", v.RequestID)
+			if v.Error != nil {
+				logger.LogAttrs(c.Request().Context(), slog.LevelError, "REQUEST_ERROR",
+					slog.Int("status", v.Status),
+					slog.String("path", v.URIPath),
+					slog.String("id", v.RequestID),
+					slog.String("method", v.Method),
+					slog.Int("latency", int(v.Latency)),
+					slog.String("agent", v.UserAgent),
+					slog.String("ip", v.RemoteIP),
+					slog.String("error", v.Error.Error()),
+				)
+			} else {
+				logger.LogAttrs(c.Request().Context(), slog.LevelError, "REQUEST",
+					slog.Int("status", v.Status),
+					slog.String("path", v.URIPath),
+					slog.String("id", v.RequestID),
+					slog.String("method", v.Method),
+					slog.Int("latency", int(v.Latency)),
+					slog.String("agent", v.UserAgent),
+					slog.String("ip", v.RemoteIP),
+				)
+			}
+			return nil
+		},
+	}))
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "hello world")
 	})
