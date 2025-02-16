9f78f89c906ffb8af40bd05d573e364a362b1828
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2025-02-16 13:35:02 +0700

    feat(register): handling duplicate constraint error

--- api/user/repository.go
+++ api/user/repository.go
@@ -5,6 +5,10 @@ import (
 	"database/sql"
 	"errors"
 	"log/slog"
+	"net/http"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/zulfikarrosadi/code_roast/lib"
 )
 
 type RepositoryImpl struct {
@@ -19,6 +23,14 @@ func (repository *RepositoryImpl) Create(ctx context.Context, user User) (User, 
 		user.Id, user.Fullname, user.Email, user.Password, user.CreatedAt,
 	)
 	if err != nil {
+		var mysqlErr *mysql.MySQLError
+		if errors.As(err, &mysqlErr) && mysqlErr.Number == DUPLICATE_CONSTRAINT_ERROR {
+			repository.Logger.LogAttrs(ctx, slog.LevelError, "duplicate email", slog.Any("details", err))
+			return user, lib.AuthError{
+				Msg:  "this email is already registered, try login instead",
+				Code: http.StatusBadRequest,
+			}
+		}
 		repository.Logger.LogAttrs(ctx, slog.LevelError, "fail insert new user", slog.Any("details", err))
 		return user, errors.New("something went wrong, please try again later")
 	}
