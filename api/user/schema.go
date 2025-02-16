0875f76fec4012a084b69c79663251cf81210bb4
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2025-02-16 13:27:07 +0700

    feat(auth): add request and response auth data

--- api/user/schema.go
+++ api/user/schema.go
@@ -7,3 +7,27 @@ type User struct {
 	Password  string `json:"password"`
 	CreatedAt int64  `json:"created_at"`
 }
+
+type userCreateRequest struct {
+	Id       string `json:"id"`
+	Fullname string `json:"fullname"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type userCreateResponse struct {
+	ID       string `json:"id"`
+	Email    string `json:"email"`
+	Fullname string `json:"fullname"`
+}
+
+type authResponse struct {
+	User         userCreateResponse `json:"user"`
+	AccessToken  string             `json:"accessToken"`
+	RefreshToken string             `json:"refreshToken"`
+}
+
+type userLoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
