c62e9518205200c1a6ecbecd32a5093cfdc11bf9
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2025-02-18 13:56:13 +0700

    feat(multi auth): embed Authentication schema to userCreqteRequest

--- api/user/schema.go
+++ api/user/schema.go
@@ -8,11 +8,22 @@ type User struct {
 	CreatedAt int64  `json:"created_at"`
 }
 
+type Authentication struct {
+	Id           string `json:"id"`
+	RefreshToken string `json:"refresh_token"`
+	LastLogin    int64  `json:"last_login"`
+	RemoteIP     string `json:"remote_ip"`
+	Agent        string `json:"agent"`
+	UserId       string `json:"user_id"`
+}
+
 type userCreateRequest struct {
-	Id       string `json:"id"`
-	Fullname string `json:"fullname"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Id        string `json:"id"`
+	Fullname  string `json:"fullname"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	CreatedAt int64  `json:"created_at"`
+	Authentication
 }
 
 type userCreateResponse struct {
