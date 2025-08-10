ef7ac924ee925560eebe0d72d8182fb3d89d8f10
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2025-08-10 12:29:27 +0700

    feat(navigation): update navigation for authenticated user

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -3,8 +3,13 @@
   import { Toaster } from '$lib/components/ui/sonner/index.js'
   import { toast } from 'svelte-sonner'
   import { goto } from '$app/navigation'
+  import { BellIcon } from '@lucide/svelte'
 
   let { data, children } = $props()
+
+  $effect(() => {
+    $inspect(data)
+  })
 
   async function handleSignOut(event: Event) {
     event.preventDefault()
@@ -29,25 +34,30 @@
   }
 </script>
 
-<header class="flex h-12 w-full items-center border-b-2">
+<header class="flex h-18 w-full items-center border-b-2">
   <nav class="mx-auto flex w-10/12 items-center justify-between">
     <div>
       <a href="/">Code Roast</a>
     </div>
     <div class="flex gap-4">
-      {#if data.user}
-        <a href="/post">Post</a>
-        <div>
-          <form action="/signout" onsubmit={(event) => handleSignOut(event)}>
-            <button class="cursor-pointer">Sign Out</button>
-          </form>
-        </div>
-        <p class="font-bold">{data.user.fullname}</p>
-      {:else}
+      <a href="/post">Post</a>
+      <a href="/event">Event</a>
+      <a href="/subforum">Subforum</a>
+      {#if !data.user}
         <a href="/signup">Sign Up</a>
         <a href="/signin">Sign In</a>
       {/if}
     </div>
+
+    {#if data.user}
+      <div class="flex gap-4 items-center">
+        <BellIcon size={16} aria-label="notification" />
+        <p class="font-bold">{data.user.fullname}</p>
+        <form action="/signout" onsubmit={(event) => handleSignOut(event)}>
+          <button class="cursor-pointer">Sign Out</button>
+        </form>
+      </div>
+    {/if}
   </nav>
 </header>
 
